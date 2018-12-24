9d0e178d7cd5b4689bb9d28fd74a7ab2fdf2a89f
aljoebacus <aljoebacus@gmail.com>
2018-12-24 15:08:20 -0700

    Added basic code structure for components and barebones HTML & CSS code for SearchBar

---
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './CommentSection.js';
+
+const CommentSection = pros => {
+  return (
+    <input className="ig-clone__search-bar" placeholder="Search"/>  
+  );
+}

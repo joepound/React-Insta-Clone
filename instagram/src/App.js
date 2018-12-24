0e2e9fbf90e9246b2e713552f02e257ed180bc7c
aljoebacus <aljoebacus@gmail.com>
2018-12-24 16:16:24 -0700

    Added component for posts (without comments section as of now)

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -3,18 +3,20 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
-// import PostContainer from './components/PostContainer/PostContainer.js';
+import PostContainer from './components/PostContainer/PostContainer.js';
 // import CommentSection from './components/CommentSection/CommentSection.js';
 
 class App extends Component {
   constructor() {
     super();
+    this.igData = dummyData;
   }
 
   render() {
     return (
       <main className="ig-clone-container">
         <SearchBar />
+        {this.igData.map(data => <PostContainer key={data.id} igPostData={data} />)}
       </main>
     );
   }
--- instagram/src/components/PostContainer/PostContainer.js
+++ instagram/src/components/PostContainer/PostContainer.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 
 import './PostContainer.css';
 
+import HeartIcon from '../../heart.png';
+import CommentIcon from '../../comment.png';
+
 class PostContainer extends Component {
-    constructor (props) {
+  constructor(props) {
+    super();
+  }
 
-    }
-
-    render() {
-        return (
-
-        );
-    }
+  render() {
+    return (
+      <article className="ig-clone__post">
+        <header className="ig-clone__post__poster-info">
+          <div className="ig-clone__post__poster-info__dp"><img src={this.props.igPostData.thumbnailUrl}/></div>
+          <span className="ig-clone__post__poster-info__acct">{this.props.igPostData.username}</span>
+        </header>
+        <figure className="ig-clone__post__photo">
+          <img src={this.props.igPostData.imageUrl}/>
+        </figure>
+        <div className="ig-clone__post__icons">
+          <input type="image" src={HeartIcon} alt="Like this photo"/>
+          <input type="image" src={CommentIcon} alt="Comment on this photo"/>
+        </div>
+        <div className="ig-clone__post__likes"><span className="ig-clone__post__likes__count">{this.props.igPostData.likes}</span> likes</div>
+      </article>
+    );
+  }
 }
 
 export default PostContainer;

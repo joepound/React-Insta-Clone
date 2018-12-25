692f302c401229fa25559698a95cd89459010323
joepound <14357797+joepound@users.noreply.github.com>
2018-12-24 22:00:22 -0700

    Added more content to top nav/search bar and fixed it to top of page

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -2,9 +2,26 @@ import React from 'react';
 
 import './SearchBar.css';
 
+import InstagramIcon from './../../instagram.png';
+import DiscoverIcon from './../../discover.png';
+import HeartIcon from './../../heart.png';
+import UserIcon from './../../user.png';
+
 const SearchBar = props => {
   return (
-    <input className="ig-clone__search-bar" placeholder="Search"/>
+    <nav className="ig-clone__search-bar">
+      <div className="ig-clone__search-bar__ig-logos">
+        <input className="ig-clone__search-bar__ig-logos__icon" type="image" src={InstagramIcon} alt="Instagram Home"/> | <span className="ig-clone__search-bar__ig-logos__text">Instagram</span>
+      </div>
+      <div className="ig-clone__search-bar__container">
+        <input className="ig-clone__search-bar__input" placeholder="Search"/>
+      </div>
+      <div className="ig-clone__search-bar__nav-icons">
+        <input type="image" src={DiscoverIcon} alt="Discover more friends"/>
+        <input type="image" src={HeartIcon} alt="Notifications"/>
+        <input type="image" src={UserIcon} alt="Your profile"/>
+      </div>
+    </nav>
   );
 };
 
